d330131e1dca7b6f92d8104a689e247b8e95d900
Ben Simner <benjsimner@gmail.com>
2020-02-04 13:45:06 +0000

    Add copy of MP+dmb+svc test

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include "lib.h"
 
 extern void MyMP_pos(void);
+extern void MyMP_dmb_svc(void);
 
 int main(void) {
-    puts("Hello, World!\n");
-
-    NUMBER_OF_RUNS = 10000;
+    NUMBER_OF_RUNS = 100000UL;
     MyMP_pos();
+    MyMP_dmb_svc();
     return 0;
 }
