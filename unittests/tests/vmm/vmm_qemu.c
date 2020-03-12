97220a4302071087bce11fa7b18151306b7de921
Ben Simner <benjsimner@gmail.com>
2020-03-12 22:45:47 +0000

    Add QEMU unittests

---
+++ unittests/tests/vmm/vmm_qemu.c
@@ -0,0 +1,18 @@
+#include "lib.h"
+
+#include "testlib.h"
+
+UNIT_TEST(test_vmm_qemu_uart)
+void test_vmm_qemu_uart(void) {
+  uint64_t* pgtable = vmm_alloc_new_idmap_4k();
+  ASSERT((uint64_t)vmm_pa(pgtable, UART0_BASE) == UART0_BASE);
+}
+
+UNIT_TEST(test_vmm_qemu_uart_translated)
+void test_vmm_qemu_uart_translated(void) {
+  uint64_t* pgtable = vmm_alloc_new_idmap_4k();
+  vmm_set_id_translation(pgtable);
+  writeb('\0', UART0_BASE);
+  vmm_set_id_translation(NULL);
+  ASSERT(1);  /* assert it *reaches* end of test */
+}
