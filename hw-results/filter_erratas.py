20045b31e0b0b1adb30e74ac38f70693b1573bcd
Ben Simner <ben.simner@cl.cam.ac.uk>
2020-08-18 18:04:00 +0100

    Clean filter_erratas script

--- hw-results/filter_erratas.py
+++ hw-results/filter_erratas.py
@@ -6,13 +6,9 @@ import collections
 
 import yaml
 
-def read_date(date):
-    formats = [
-        "%Y-%B-%d",
-        "%Y-%b-%d",
-        "%Y-%j",
-        "%Y-"
-    ]
+
+def parse_date(date):
+    formats = ["%Y-%B-%d", "%Y-%b-%d", "%Y-%j", "%Y-"]
 
     for fmt in formats:
         try:
@@ -22,6 +18,32 @@ def read_date(date):
 
     return None
 
+
+def parse_file_date(fname):
+    m = re.search(r"(\d{4})-overnight-(\d{1,3})", fname)
+    if m:
+        return parse_date("{}-{}".format(m.group(1), m.group(2)))
+
+    m = re.search(r"(\d{4})-(.+)-(\d+)-.+", fname)
+    if m:
+        return parse_date("{}-{}-{}".format(m.group(1), m.group(2), m.group(3)))
+
+
+def read_all_errata():
+    with open("../errata.yaml", "r", encoding="utf-8") as f:
+        content = yaml.load(f, Loader=yaml.SafeLoader)
+
+    errata = collections.defaultdict(set)
+    for item in content:
+        date = parse_date(item["date"])
+        tests = [it["test"] for it in item["errata"]]
+        for test_list in tests:
+            for test in test_list.split():
+                errata[date].add(test)
+
+    return errata
+
+
 def subs(fpath, tests):
     with open(fpath, "r", encoding="utf-8") as f:
         content = f.read()
@@ -29,51 +51,32 @@ def subs(fpath, tests):
     for test in tests:
         if test.startswith("@"):
             if test == "@all":
-                content = re.sub(r"Observation (.+):", r"Observation \g<1>.errata:", content)
+                content = re.sub(
+                    r"Observation (.+):", r"Observation \g<1>.errata:", content
+                )
             else:
                 sys.exit("unsupported errata for group {}".format(test))
         else:
-            content = content.replace(f"Observation {test}:", f"Observation {test}.errata:")
+            content = content.replace(
+                f"Observation {test}:", f"Observation {test}.errata:"
+            )
 
     with open(fpath, "w", encoding="utf-8") as f:
         f.write(content)
 
-def read_file_date(fname):
-    m = re.search(r"(\d{4})-overnight-(\d{1,3})", fname)
-    if m:
-        return read_date("{}-{}".format(m.group(1), m.group(2)))
 
-    m = re.search(r"(\d{4})-(.+)-(\d+)-.+", fname)
-    if m:
-        return read_date("{}-{}-{}".format(m.group(1), m.group(2), m.group(3)))
-
-
-def read_erratas() -> yaml.YAMLObject:
-    with open('../errata.yaml', 'r', encoding='utf-8') as f:
-        content = yaml.load(f, Loader=yaml.SafeLoader)
-
-    errata = collections.defaultdict(set)
-    for item in content:
-        date = read_date(item['date'])
-        tests = [it['test'] for it in item['errata']]
-        for test_list in tests:
-            for test in test_list.split():
-                errata[date].add(test)
-
-    return errata
-
-def filter_erratas(dir):
+def filter_erratas(errata, dir):
     for f in dir.iterdir():
-        date = read_file_date(f.name)
+        date = parse_file_date(f.name)
 
         for d in errata.keys():
             if date is None or date <= d:
                 subs(f, errata[d])
 
+
 cwd = pathlib.Path(".")
 
-errata = read_erratas()
-
-filter_erratas(cwd/"rpi4b")
-filter_erratas(cwd/"rpi3bp")
-filter_erratas(cwd/"graviton2")
+errata = read_all_errata()
+filter_erratas(errata, cwd / "rpi4b")
+filter_erratas(errata, cwd / "rpi3bp")
+filter_erratas(errata, cwd / "graviton2")
