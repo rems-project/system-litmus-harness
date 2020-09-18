4ca6e886de09b3e15347488f7ab162e9a1141c25
Ben Simner <ben.simner@cl.cam.ac.uk>
2020-09-18 10:15:23 +0100

    Each region 1 PMD

--- inc/litmus/litmus_regions.h
+++ inc/litmus/litmus_regions.h
@@ -24,10 +24,18 @@ typedef struct {
   page_t pages[NR_PAGES_PER_DIR];
 } dir_t;
 
-/** a 8M region
- * is split into 4 middle-level dirs
- * each of which contains 512 pages
- * which each contain 4k bytes of data.
+/** each region is one pmd (or page-middle-directory)
+ * each pmd entry points to 1 2M region, so each pmd is 1G aligned
+ *
+ * we ask QEMU to allocate 1 G of mem right now
+ * but we cannote use all of that for the regions
+ * so we cannot allocate a whole 1G of of dirs
+ *
+ * instead owe allocate 8 M which is more than enough for the current tests
+ *
+ * For now this region is physically contiguous, in future we may
+ * want to make this a virtual array where only some of the points
+ * are mapped in, allowing tests over a wider range of VAs.
  */
 #define NR_DIRS_PER_REGION 4
 #define REGION_SIZE (NR_DIRS_PER_REGION * NR_PAGES_PER_DIR * PAGE_SIZE)
@@ -37,9 +45,9 @@ typedef struct {
 
 /** heap variable data is split over regions
  *
- * each region covers 1 8M section of memory
+ * each region covers a pud (or page-upper-directory)
  */
-#define NR_REGIONS 8
+#define NR_REGIONS 1
 typedef struct {
   region_t regions[NR_REGIONS];
 } regions_t;
