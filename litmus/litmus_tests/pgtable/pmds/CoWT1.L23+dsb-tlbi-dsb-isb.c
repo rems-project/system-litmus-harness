1b45e91168c9f3afebe3f4471bfb0d7f32f9e01a
Ben Simner <ben.simner@cl.cam.ac.uk>
2020-09-21 17:15:25 +0100

    New translation tests

---
+++ litmus/litmus_tests/pgtable/pmds/CoWT1.L23+dsb-tlbi-dsb-isb.c
@@ -0,0 +1,53 @@
+#include <stdint.h>
+
+#include "lib.h"
+
+#define VARS x, y
+#define REGS p0x2
+
+static void P0(litmus_test_run* data) {
+  asm volatile (
+    /* move from C vars into machine regs */
+      "mov x0, %[ypmddesc]\n\t"
+      "mov x1, %[xpmd]\n\t"
+      "mov x3, %[x]\n\t"
+      "mov x4, %[xpage]\n\t"
+
+      /* test */
+      "str x0, [x1]\n\t"
+      "dsb sy\n\t"
+      "tlbi vae1,x4\n\t"
+      "dsb sy\n\t"
+      "isb\n\t"
+      "ldr x2, [x3]\n\t"
+
+      /* output back to C vars */
+      "str x2, [%[outp0r2]]\n\t"
+  :
+  : ASM_VARS(data, VARS),
+    ASM_VAR_PMDs(data, VARS),
+    ASM_VAR_PMDDESCs(data, VARS),
+    ASM_REGS(data, REGS)
+  : "cc", "memory", "x0", "x1", "x2", "x3", "x4"
+  );
+}
+
+litmus_test_t CoWT1L23_dsbtlbidsbisb = {
+  "CoWT1.L23+dsb-tlbi-dsb-isb",
+  MAKE_THREADS(1),
+  MAKE_VARS(VARS),
+  MAKE_REGS(REGS),
+  INIT_STATE(
+    4,
+    INIT_VAR(x, 0),
+    INIT_VAR(y, 1),
+    INIT_REGION_OWN(x, REGION_OWN_PMD),
+    INIT_REGION_OFFSET(y, x, REGION_SAME_PMD_OFFSET),
+  ),
+  .interesting_result = (uint64_t[]){
+      /* p0:x2 =*/0,
+  },
+  .start_els = (int[]){1,}, /* start at El1 for TLBI */
+  .requires_pgtable = 1,
+  .no_sc_results = 1,
+};
---
+++ litmus/litmus_tests/pgtable/same_page/CoTT2+po-po.c
@@ -0,0 +1,50 @@
+#include <stdint.h>
+
+#include "lib.h"
+
+#define VARS x1, x2, y1, y2
+#define REGS p0x2, p0x4
+
+static void P0(litmus_test_run* data) {
+  asm volatile (
+    /* move from C vars into machine regs */
+    "mov x0, %[y1desc]\n\t"
+    "mov x1, %[x1pte]\n\t"
+    "mov x3, %[x1]\n\t"
+    "mov x5, %[x2]\n\t"
+    /* test */
+    "str x0, [x1]\n\t"
+    "ldr x2, [x3]\n\t"
+    "ldr x4, [x5]\n\t"
+    /* output */
+    "str x2, [%[outp0r2]]\n\t"
+    "str x4, [%[outp0r4]]\n\t"
+  :
+  : ASM_VARS(data, VARS),
+    ASM_REGS(data, REGS)
+  : "cc", "memory", "x0", "x1", "x2", "x3", "x4", "x5"
+  );
+}
+
+litmus_test_t CoTT2_popo = {
+  "CoTT2+po-po",
+  MAKE_THREADS(1),
+  MAKE_VARS(VARS),
+  MAKE_REGS(REGS),
+  INIT_STATE(
+    7,
+    INIT_VAR(x1, 0),
+    INIT_VAR(x2, 0),
+    INIT_VAR(y1, 1),
+    INIT_VAR(y2, 1),
+    INIT_REGION_PIN(x2, x1, REGION_SAME_PAGE),
+    INIT_REGION_PIN(y2, y1, REGION_SAME_PAGE),
+    INIT_REGION_OFFSET(y2, x2, REGION_SAME_PAGE_OFFSET),
+  ),
+  .interesting_result = (uint64_t[]){
+      /* p0:x2 =*/1,
+      /* p0:x4 =*/0,
+  },
+  .requires_pgtable = 1,
+  .no_sc_results = 3,
+};
