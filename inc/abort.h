5479c2c6636666554879ed9cc543b33317a328a2
bjs <benjsimner@gmail.com>
2024-03-06 11:39:14 +0000

    (inc/abort) add fail_on macro for conditional checks

--- inc/abort.h
+++ inc/abort.h
@@ -13,4 +13,10 @@ void abort(void);
     abort(); \
   } while (0)
 
+#define fail_on(COND, ...) \
+  if ((COND)) { \
+    printf_with_fileloc("abort", 1, __FILE__, __LINE__, __FUNCTION__, ##__VA_ARGS__); \
+    raise_to_el1(); \
+    abort(); \
+  }
 #endif /* ABORT_H */
