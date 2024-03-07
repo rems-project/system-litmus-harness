7fb4307d4140f2cf8bddb12d8980494f4da3a95c
bjs <benjsimner@gmail.com>
2024-03-07 15:39:59 +0000

    (unittests/valloc) add tests for maximumal and minimal alignment

--- unittests/tests/valloc/valloc_alignment.c
+++ unittests/tests/valloc/valloc_alignment.c
@@ -5,7 +5,25 @@
 
 UNIT_TEST(test_valloc_alloc_aligned)
 void test_valloc_alloc_aligned(void) {
+  /* using just alloc() the argument might not be fully-aligned */
   char* p = alloc(1024);
   FREE(p);
+  ASSERT(IS_ALIGNED((u64)p, 3));
+}
+
+UNIT_TEST(test_valloc_alloc_with_alignment_is_aligned)
+void test_valloc_alloc_with_alignment_is_aligned(void) {
+  /* using just alloc() the argument might not be aligned */
+  char* p = alloc_with_alignment(1024, 1024);
+  FREE(p);
   ASSERT(IS_ALIGNED((u64)p, 10));
+}
+
+UNIT_TEST(test_valloc_alloc_minimal_alignment)
+void test_valloc_alloc_minimal_alignment(void) {
+  /* using just alloc() the argument might not be aligned */
+  char* p = alloc(1);
+  FREE(p);
+  /* but it should have at least a minimum alignment of 64-bits */
+  ASSERT(IS_ALIGNED((u64)p, 3));
 }
