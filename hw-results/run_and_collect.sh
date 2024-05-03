866e14e0dda862f8728b09b3115d5d19cdc38c30
bjs <benjsimner@gmail.com>
2024-05-03 16:30:46 +0000

    (hw-results/run) direct logs to dedicated logs/ subdir

--- hw-results/run_and_collect.sh
+++ hw-results/run_and_collect.sh
@@ -3,7 +3,7 @@
 #
 # Runs kvm_litmus with the provided args, in quiet mode
 #      with a default run and batch size
-#      directing the output to hw-refs/device_name/YYYY-MM-DD.log
+#      directing the output to hw-refs/device_name/logs/YYYY-MM-DD.log
 #
 # Example: ./run_and_collect.sh MP+pos --run-forever
 
@@ -12,5 +12,6 @@ HARNESS_ROOT=$(dirname ${HERE})
 KVM_LITMUS=${HARNESS_ROOT}/kvm_litmus
 TS=$(date -I)
 RESULTS_DIR=${HERE}/hw-refs/$(hostname)
-mkdir -p ${RESULTS_DIR}
-nohup ${KVM_LITMUS} -q -n500k -b2k $@ > ${RESULTS_DIR}/${TS}.log &
+mkdir -p ${RESULTS_DIR}/logs
+${KVM_LITMUS} --id > ${RESULTS_DIR}/id.txt
+nohup ${KVM_LITMUS} -q -n500k -b2k $@ > ${RESULTS_DIR}/logs/${TS}.log &
