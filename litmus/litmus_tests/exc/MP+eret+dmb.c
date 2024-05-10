bf73b930cfea15bbf3180a072e1d3d09a5cc223f
bjs <benjsimner@gmail.com>
2024-05-10 21:04:32 +0000

    (litmus_tests/exc) add some more MP svc/eret shapes

---
+++ litmus/litmus_tests/exc/MP+eret+dmb.c
@@ -0,0 +1,70 @@
+#include "lib.h"
+
+#define VARS x, y
+#define REGS p1x0, p1x2
+
+static void svc_handler0(void) {
+  asm volatile(
+    /* write data */
+    "mov x0, #1\n\t"
+    "str x0, [x1]\n\t"
+
+    /* try to synchronise with eret */
+    "eret\n\t"
+  );
+}
+
+static void P0(litmus_test_run* data) {
+  asm volatile(
+    "mov x1, %[x]\n\t"
+    "mov x3, %[y]\n\t"
+
+    /* go start at EL1 */
+    "svc #0\n\t"
+
+    /* write flag */
+    "mov x2, #1\n\t"
+    "str x2, [x3]\n\t"
+    :
+    : ASM_VARS(data, VARS), ASM_REGS(data, REGS)
+    : "cc", "memory", "x0", "x1", "x2", "x3"
+  );
+}
+
+static void P1(litmus_test_run* data) {
+  asm volatile(
+    /* initial registers */
+    "mov x1, %[y]\n\t"
+    "mov x3, %[x]\n\t"
+
+    /* read flag */
+    "ldr x0, [x1]\n\t"
+    "dmb sy\n\t"
+    "ldr x2, [x3]\n\t"
+
+    /* extract values */
+    "str x0, [%[outp1r0]]\n\t"
+    "str x2, [%[outp1r2]]\n\t"
+    :
+    : ASM_VARS(data, VARS), ASM_REGS(data, REGS)
+    : "cc", "memory", "x0", "x1", "x2", "x3", "x4", "x5", "x6", "x7" /* dont touch parameter registers */
+  );
+}
+
+litmus_test_t MP_eret_dmb = {
+  "MP+eret+dmb",
+  MAKE_THREADS(2),
+  MAKE_VARS(VARS),
+  MAKE_REGS(REGS),
+  INIT_STATE(2, INIT_VAR(x, 0), INIT_VAR(y, 0)),
+  .thread_sync_handlers =
+    (u32 * *[]){
+      (u32*[]){ (u32*)svc_handler0, NULL },
+      (u32*[]){ NULL, NULL },
+    },
+  .interesting_result = (u64[]){
+    /* p1:x0 =*/1,
+    /* p1:x2 =*/0,
+  },
+  .no_sc_results = 3,
+};
