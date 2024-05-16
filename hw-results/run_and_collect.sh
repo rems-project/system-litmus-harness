20a56e4057811f967f2371208c59661f9b43707d
bjs <benjsimner@gmail.com>
2024-05-16 21:02:08 +0000

    (hw-results/run_and_collect) add HWREF_NAME envvar

--- hw-results/run_and_collect.sh
+++ hw-results/run_and_collect.sh
@@ -5,13 +5,17 @@
 #      with a default run and batch size
 #      directing the output to hw-refs/device_name/logs/YYYY-MM-DD.log
 #
+# device_name is the HWREF_NAME env var, or simply the machine hostname if
+#             that does not exist.
+#
 # Example: ./run_and_collect.sh MP+pos --run-forever
+# Example: HWREF_NAME=myResults ./run_and_collect.sh MP+pos --run-forever
 
 HERE=$(dirname "$0")
 HARNESS_ROOT=$(dirname ${HERE})
 KVM_LITMUS=${HARNESS_ROOT}/kvm_litmus
 TS=$(date -I)
-RESULTS_DIR=${HERE}/hw-refs/$(hostname)
+RESULTS_DIR=${HERE}/hw-refs/${HWREF_NAME:-$(hostname)}
 mkdir -p ${RESULTS_DIR}/logs
 ${KVM_LITMUS} --id > ${RESULTS_DIR}/id.txt
 nohup ${KVM_LITMUS} -q -n500k -b2k $@ >> ${RESULTS_DIR}/logs/${TS}.log &
