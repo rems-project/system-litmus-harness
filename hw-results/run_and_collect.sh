dcf680ac6b49ba1a95be82294446aee42614dd54
bjs <benjsimner@gmail.com>
2024-05-17 11:28:11 +0000

    (hw-results/run) add coloured log print

--- hw-results/run_and_collect.sh
+++ hw-results/run_and_collect.sh
@@ -11,11 +11,27 @@
 # Example: ./run_and_collect.sh MP+pos --run-forever
 # Example: HWREF_NAME=myResults ./run_and_collect.sh MP+pos --run-forever
 
+# ANSI escapes for nice coloured text
+ESC='\033'
+CSI='['
+RESET="${ESC}${CSI}0m"
+YELLOW="${ESC}${CSI}0;33m"
+RED="${ESC}${CSI}0;31m"
+
 HERE=$(dirname "$0")
 HARNESS_ROOT=$(dirname ${HERE})
 KVM_LITMUS=${HARNESS_ROOT}/kvm_litmus
 TS=$(date -I)
 RESULTS_DIR=${HERE}/hw-refs/${HWREF_NAME:-$(hostname)}
+
+# write h/w identification
+if ! ${KVM_LITMUS} --id > ${RESULTS_DIR}/id.txt; then
+  printf "${RED}[run_and_collect] Failed to generate id.txt${RESET}\n"
+  exit 1
+fi
+
 mkdir -p ${RESULTS_DIR}/logs
-${KVM_LITMUS} --id > ${RESULTS_DIR}/id.txt
-nohup ${KVM_LITMUS} -q -n500k -b2k $@ >> ${RESULTS_DIR}/logs/${TS}.log &
+LOG=${RESULTS_DIR}/logs/${TS}.log
+
+printf "${YELLOW}[run_and_collect] Storing results to ${LOG}${RESET}\n"
+nohup ${KVM_LITMUS} -q -n500k -b2k $@ 2>&1 >> ${LOG} </dev/null &
